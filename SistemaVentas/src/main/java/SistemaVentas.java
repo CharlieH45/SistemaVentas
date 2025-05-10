82bc7d993d4291cb1a4e4276a04a1b9d1a5298e3
Mufasa <Mufasa@DESKTOP-990U8KO>
2025-05-10 17:33:55 -0500

    Creacion proyecto

---
+++ SistemaVentas/src/main/java/SistemaVentas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Mufasa
+ */
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
